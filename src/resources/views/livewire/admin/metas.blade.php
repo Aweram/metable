967fd2c32a38edc9244629b33a717add71ebc98c
Labrock <vladimirpeshkov5@yandex.ru>
2024-06-13 05:45:20 +0300

    add card title

--- src/resources/views/livewire/admin/metas.blade.php
+++ src/resources/views/livewire/admin/metas.blade.php
@@ -1,6 +1,9 @@
 <div class="row">
     <div class="col w-full">
         <div class="card">
+            <div class="card-header">
+                <h2 class="font-medium text-2xl">{{ __("Meta tags") }}</h2>
+            </div>
             <div class="card-body">
                 @include("ma::admin.includes.meta-search")
                 <x-tt::notifications.error />
