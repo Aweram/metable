4fbf937a1cfb1b2de960229f5f7b8e237dbc21fb
Labrock <vladimirpeshkov5@yandex.ru>
2024-06-20 05:27:52 +0300

    fix card header

--- src/resources/views/livewire/admin/metas.blade.php
+++ src/resources/views/livewire/admin/metas.blade.php
@@ -2,15 +2,15 @@
     <div class="col w-full">
         <div class="card">
             <div class="card-header border-b-0">
-                <div class="flex justify-between items-center">
-                    <h2 class="font-medium text-2xl">{{ __("Meta tags") }}</h2>
+                <div class="space-y-indent-half">
+                    <div class="flex justify-between items-center">
+                        <h2 class="font-medium text-2xl">{{ __("Meta tags") }}</h2>
 
-                    @include("ma::admin.includes.meta-search")
+                        @include("ma::admin.includes.meta-search")
+                    </div>
+                    <x-tt::notifications.error />
+                    <x-tt::notifications.success />
                 </div>
-            </div>
-            <div class="space-y-indent-half">
-                <x-tt::notifications.error />
-                <x-tt::notifications.success />
             </div>
 
             @include("ma::admin.includes.meta-table")
